11f257761cf54ce5e34b0b056abffcdd1af93748
wlwlxj@gmail.com <wlwlxj@gmail.com@b2b8c3b8-ce47-b78c-ec54-380d862a5473>
2011-05-26 03:05:02 +0000

    添加树本地控件
    git-svn-id: http://x-framework.googlecode.com/svn/trunk@74 b2b8c3b8-ce47-b78c-ec54-380d862a5473

---
+++ library/view/controls/tree/tree_model.cpp
@@ -0,0 +1,20 @@
+
+#include "tree_model.h"
+
+#include "base/logging.h"
+
+namespace view
+{
+
+    void TreeModel::SetTitle(TreeModelNode* node,
+        const string16& title)
+    {
+        NOTREACHED();
+    }
+
+    int TreeModel::GetIconIndex(TreeModelNode* node)
+    {
+        return -1;
+    }
+
+} //namespace view
