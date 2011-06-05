1096b6930624aeb57f3590e839f3a150c374da97
wlwlxj@gmail.com <wlwlxj@gmail.com@b2b8c3b8-ce47-b78c-ec54-380d862a5473>
2011-06-05 01:44:14 +0000

    base库不完全更新至r86235
    git-svn-id: http://x-framework.googlecode.com/svn/trunk@99 b2b8c3b8-ce47-b78c-ec54-380d862a5473

---
+++ library/base/memory/scoped_open_process.h
@@ -0,0 +1,55 @@
+
+#ifndef __base_memory_scoped_open_process_h__
+#define __base_memory_scoped_open_process_h__
+
+#pragma once
+
+#include "../process.h"
+#include "../process_util.h"
+
+namespace base
+{
+
+    // A class that opens a process from its process id and closes it when the
+    // instance goes out of scope.
+    class ScopedOpenProcess
+    {
+    public:
+        ScopedOpenProcess() : handle_(kNullProcessHandle) {}
+
+        // Automatically close the process.
+        ~ScopedOpenProcess()
+        {
+            Close();
+        }
+
+        // Open a new process by pid. Closes any previously opened process (even if
+        // opening the new one fails).
+        bool Open(ProcessId pid)
+        {
+            Close();
+            return OpenProcessHandle(pid, &handle_);
+        }
+
+        // Close the previously opened process.
+        void Close()
+        {
+            if(handle_ == kNullProcessHandle)
+            {
+                return;
+            }
+
+            CloseProcessHandle(handle_);
+            handle_ = kNullProcessHandle;
+        }
+
+        ProcessHandle handle() const { return handle_; }
+
+    private:
+        ProcessHandle handle_;
+        DISALLOW_COPY_AND_ASSIGN(ScopedOpenProcess);
+    };
+
+} //namespace base
+
+#endif //__base_memory_scoped_open_process_h__
