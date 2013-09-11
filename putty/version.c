fe016eeba6ba9430703e4dc6738b3c66cef35538
LuDong <noodle1983@126.com>
2013-09-11 10:00:14 +0800

    putty-nd2.5
    Change-Id: Ib1ddb217ce6b99bd1be3c574ddfecb81dd737fe5

--- putty/version.c
+++ putty/version.c
@@ -30,7 +30,7 @@ char sshver[] = "PuTTY-Custom-r" STR(SVN_REV);
 
 #else
 
-char ver[] = "Noodle build 2.4, " __DATE__ " " __TIME__;
+char ver[] = "Noodle build 2.5, " __DATE__ " " __TIME__;
 char sshver[] = "PuTTY-Local: " __DATE__ " " __TIME__;
 
 #endif
