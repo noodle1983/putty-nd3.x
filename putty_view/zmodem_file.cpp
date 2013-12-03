7cdc22c0f845f98cf50d1861e74ebcf8476bd998
LuDong <noodle1983@126.com>
2013-12-03 11:57:25 +0800

    int overflow
    Change-Id: Ie885fd1e64f4fccb70682d40649ae9e227b1843a

--- putty_view/zmodem_file.cpp
+++ putty_view/zmodem_file.cpp
@@ -128,7 +128,7 @@ void ZmodemFile::setPos(unsigned pos)
 std::string ZmodemFile::getProgressLine()
 {
 	char buf[128] = {0};
-	_snprintf(buf, sizeof(buf), " %d/%d(%d%%)", pos_, file_size_, pos_*100/file_size_);
+	_snprintf(buf, sizeof(buf), " %d/%d(%d%%)", pos_, file_size_, int(1.0*100*pos_/file_size_));
 	return prompt_ + buf;
 }
 
