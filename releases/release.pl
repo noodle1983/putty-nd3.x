b244497ce6cf49051956ad5d1a76fd765e28abdb
LuDong <noodle1983@126.com>
2013-12-18 17:01:52 +0800

    change release file
    Change-Id: I9c352265f45261996129eb3ffb685ca12135e614

--- releases/release.pl
+++ releases/release.pl
@@ -6,6 +6,6 @@ $version_no =~ s/^.*?([0-9\.]+).*$/\1/;
 
 print `rm "./release/putty_nd$version_no.zip" ./putty.exe`;
 print `cp ../bin/chrome-d.exe ./putty.exe`;
-print `upx.exe ./putty.exe`;
-print `zip -9 -v "./release/putty_nd$version_no.zip" putty.exe release_note.txt`;
+print `./upx.exe ./putty.exe`;
+print `./zip -9 -v "./release/putty_nd$version_no.zip" putty.exe release_note.txt`;
 print `cp release_note.txt ./release/readme.txt`;
