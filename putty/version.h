8c2431e6d3b669eae39b71f9763a2b05f084681a
LuDong <noodle1983@126.com>
2016-03-31 16:47:33 +0800

    putty-nd4.1

--- putty/version.h
+++ putty/version.h
@@ -8,6 +8,6 @@
  * default stuff used for local development runs of 'make'.
  */
 
-#define TEXTVER "Noodle build 4.0 debug, "
+#define TEXTVER "Noodle build 4.1 debug, "
 #define SSHVER "PuTTY-Local: "
-#define BINARY_VERSION 4,0,0,0
+#define BINARY_VERSION 4,1,0,0
