383452d57d262361063cafc5515e63792dcb5620
LuDong <noodle1983@126.com>
2017-04-08 12:18:05 +0800

    putty_nd-4.3

--- releases/release.pl
+++ releases/release.pl
@@ -4,8 +4,8 @@ my $version_no = `grep Noodle ../putty/version.h`;
 chomp $version_no;
 $version_no =~ s/^.*?([0-9\.]+).*$/\1/;
 
-print `rm "./release/putty_nd$version_no.zip" ./putty.exe`;
+print `rm "./release/putty_nd${version_no}_with_adb.zip" ./putty.exe`;
 print `cp ../bin/chrome-d.exe ./putty.exe`;
 print `./upx.exe ./putty.exe`;
-print `./zip -9 -v "./release/putty_nd$version_no.zip" putty.exe release_note.txt`;
+print `./zip -9 -v "./release/putty_nd${version_no}_with_adb.zip" putty.exe release_note.txt adb.exe AdbWinApi.dll AdbWinUsbApi.dll`;
 print `cp release_note.txt ./release/readme.txt`;
