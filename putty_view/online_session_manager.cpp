52476c65e275454f16df0389bde6e4248cec639e
LuDong <noodle1983@126.com>
2017-05-12 15:33:01 +0800

    add proxy

--- putty_view/online_session_manager.cpp
+++ putty_view/online_session_manager.cpp
@@ -2,6 +2,7 @@
 #include "putty.h"
 #include "../adb/AdbManager.h"
 #include <Windows.h>
+#include <Winhttp.h>
 #include "atlconv.h" 
 extern "C"{
 #include <curl/curl.h>
@@ -85,30 +86,165 @@ void OnlineSessionManager::handleClose(SocketConnectionPtr theConnection)
 
 //-----------------------------------------------------------------------------
 
+bool getProxyAddr(const string& strAddr, char* strDestAddr, const char* type)
+{
+	int nStart = strAddr.find(type);
+	if (nStart != -1)
+	{
+		nStart += strlen(type);
+		int nLen = strAddr.find(';', nStart + 1) - nStart;
+		strcpy(strDestAddr, strAddr.substr(nStart, nLen).c_str());
+		return true;
+	}
+	return false;
+}
+
+int getIEProxy(const char* host, int& proxytype, char* strAddr, bool bUserHttps)
+{
+	USES_CONVERSION;
+	bool fAutoProxy = false;
+	WINHTTP_PROXY_INFO autoProxyInfo = { 0 };
+
+	WINHTTP_AUTOPROXY_OPTIONS autoProxyOptions = { 0 };
+	WINHTTP_CURRENT_USER_IE_PROXY_CONFIG ieProxyConfig = { 0 };
+	if (WinHttpGetIEProxyConfigForCurrentUser(&ieProxyConfig))
+	{
+		if (ieProxyConfig.fAutoDetect)
+		{
+			fAutoProxy = true;
+		}
+
+		if (ieProxyConfig.lpszAutoConfigUrl != NULL)
+		{
+			fAutoProxy = true;
+			autoProxyOptions.lpszAutoConfigUrl = ieProxyConfig.lpszAutoConfigUrl;
+		}
+	}
+	else
+	{
+		// use autoproxy
+		fAutoProxy = true;
+	}
+
+	if (fAutoProxy)
+	{
+		if (autoProxyOptions.lpszAutoConfigUrl != NULL)
+		{
+			autoProxyOptions.dwFlags = WINHTTP_AUTOPROXY_CONFIG_URL;
+		}
+		else
+		{
+			autoProxyOptions.dwFlags = WINHTTP_AUTOPROXY_AUTO_DETECT;
+			autoProxyOptions.dwAutoDetectFlags = WINHTTP_AUTO_DETECT_TYPE_DHCP | WINHTTP_AUTO_DETECT_TYPE_DNS_A;
+		}
+
+		// basic flags you almost always want
+		autoProxyOptions.fAutoLogonIfChallenged = TRUE;
+
+		HINTERNET session = ::WinHttpOpen(0, // no agent string
+			WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
+			WINHTTP_NO_PROXY_NAME,
+			WINHTTP_NO_PROXY_BYPASS,
+			WINHTTP_FLAG_ASYNC);
+
+		// here we reset fAutoProxy in case an auto-proxy isn't actually
+		// configured for this url
+
+		fAutoProxy = WinHttpGetProxyForUrl(session, A2W(host), &autoProxyOptions, &autoProxyInfo);
+		if (session) WinHttpCloseHandle(session);
+	}
+
+	if (fAutoProxy)
+	{
+		// set proxy options for libcurl based on autoProxyInfo
+		//autoProxyInfo.lpszProxy
+		//curl_easy_setopt(curl,CURLOPT_PROXY,autoProxyInfo.lpszProxy);
+		if (autoProxyInfo.lpszProxy)
+		{
+			strncpy(strAddr, W2A(autoProxyInfo.lpszProxy), 256);
+			proxytype = CURLPROXY_HTTP;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	else
+	{
+		if (ieProxyConfig.lpszProxy != NULL)
+		{
+			// IE has an explicit proxy. set proxy options for libcurl here
+			// based on ieProxyConfig
+			//
+			// note that sometimes IE gives just a single or double colon
+			// for proxy or bypass list, which means "no proxy"
+			strncpy(strAddr, W2A(ieProxyConfig.lpszProxy), 256);
+			proxytype = CURLPROXY_HTTP;
+
+			///may be like this: "http=127.0.0.1:8888;https=127.0.0.1:8888;ftp=127.0.0.1:8888;socks=127.0.0.1:8888" "127.0.0.1:8888"
+			string strProxyAddr(strAddr);
+			if (strProxyAddr.find('=') != -1)
+			{
+				bool bFind = false;
+				if (bUserHttps && getProxyAddr(strProxyAddr, strAddr, "https=")) bFind = true;
+				if (bFind == false && getProxyAddr(strProxyAddr, strAddr, "http=")) bFind = true;
+				if (bFind == false && getProxyAddr(strProxyAddr, strAddr, "socks="))
+				{
+					proxytype = CURLPROXY_SOCKS5;
+				}
+			}
+		}
+		else
+		{
+			proxytype = -1;
+			// there is no auto proxy and no manually configured proxy
+		}
+	}
+
+	if (autoProxyInfo.lpszProxy != NULL) GlobalFree(autoProxyInfo.lpszProxy);
+	if (autoProxyInfo.lpszProxyBypass != NULL) GlobalFree(autoProxyInfo.lpszProxyBypass);
+	//if(autoProxyOptions.lpszAutoConfigUrl != NULL) GlobalFree(autoProxyOptions.lpszAutoConfigUrl);
+	if (ieProxyConfig.lpszAutoConfigUrl != NULL) GlobalFree(ieProxyConfig.lpszAutoConfigUrl);
+	if (ieProxyConfig.lpszProxy != NULL) GlobalFree(ieProxyConfig.lpszProxy);
+	if (ieProxyConfig.lpszProxyBypass != NULL) GlobalFree(ieProxyConfig.lpszProxyBypass);
+
+	return proxytype;
+}
+
 void OnlineSessionManager::upload_file(string file)
 {	
-	g_online_session_manager->mTcpServer.start();
-	int port = g_online_session_manager->mTcpServer.getBindedPort();
-	//CURL *curl;
-	//CURLcode res;
-	//string response_str;
-	//
-	//curl = curl_easy_init();
-	//assert(curl);
-	//curl_easy_setopt(curl, CURLOPT_URL, "https://www.taobao.com");
-	//curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "cmd=time");
-	//curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, query_auth_write_cb);
-	//curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&response_str);
-	//
-	//int count = 0;
-	//while ((res = curl_easy_perform(curl)) != CURLE_OK && count < 5)
-	//{
-	//	response_str.clear();
-	//	Sleep(1000);
-	//	count++;
-	//}
-	//curl_easy_cleanup(curl);
-	//g_ui_processor->process(NEW_PROCESSOR_JOB(OnlineSessionManager::upload_file_done, res == CURLE_OK, response_str));
+	int proxytype = 0;
+	char strAddr[256] = { 0 };
+	bool bUserHttps = true;
+	int get_proxy_return = getIEProxy("https://www.google.com", proxytype, strAddr, bUserHttps);
+
+	//g_online_session_manager->mTcpServer.start();
+	//int port = g_online_session_manager->mTcpServer.getBindedPort();
+	
+	CURL *curl;
+	CURLcode res;
+	string response_str;
+	
+	curl = curl_easy_init();
+	assert(curl);
+	curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, query_auth_write_cb);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&response_str); 
+	if (get_proxy_return >= 0)
+	{
+		curl_easy_setopt(curl, CURLOPT_PROXY, strAddr);
+		curl_easy_setopt(curl, CURLOPT_PROXYTYPE, proxytype);
+	}
+	
+	int count = 0;
+	while ((res = curl_easy_perform(curl)) != CURLE_OK && count < 5)
+	{
+		response_str.clear();
+		Sleep(1000);
+		count++;
+	}
+	curl_easy_cleanup(curl);
+	g_ui_processor->process(NEW_PROCESSOR_JOB(OnlineSessionManager::upload_file_done, res == CURLE_OK, response_str));
 }
 
 
