2407e4ca7ceff0d91e540cbd57c959ea8cb90aaf
LuDong <noodle1983@126.com>
2017-07-29 22:16:38 +0800

    5.1

--- releases/release.pl
+++ releases/release.pl
@@ -5,7 +5,7 @@ chomp $version_no;
 $version_no =~ s/^.*?([0-9\.]+).*$/\1/;
 
 print `rm "./release/putty_nd${version_no}_with_adb.zip" ./putty.exe`;
-print `cp ../bin/chrome-d.exe ./putty.exe`;
+print `cp ../bin/chrome.exe ./putty.exe`;
 print `./upx.exe ./putty.exe`;
 print `./zip -9 -v "./release/putty_nd${version_no}_with_adb.zip" putty.exe release_note.txt adb.exe AdbWinApi.dll AdbWinUsbApi.dll`;
 print `cp release_note.txt ./release/readme.txt`;
