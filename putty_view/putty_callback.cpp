af35d9e0ca8dd4ad6216e23c8ae8e70267f86d29
LuDong <noodle1983@126.com>
2018-01-16 17:51:04 +0800

    add get_ms

--- putty_view/putty_callback.cpp
+++ putty_view/putty_callback.cpp
@@ -2018,4 +2018,19 @@ void update_toolbar(bool should_restore_state)
 	WindowInterface::GetInstance()->UpdateToolbar(should_restore_state);
 }
 
+typedef unsigned long long uint64_t;
+uint64_t get_ms()
+{
+	// Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
+	static const uint64_t EPOCH = ((uint64_t)116444736000000000ULL);
 
+	SYSTEMTIME  system_time;
+	FILETIME    file_time;
+	uint64_t    time;
+
+	GetSystemTime(&system_time);
+	SystemTimeToFileTime(&system_time, &file_time);
+	time = ((uint64_t)file_time.dwLowDateTime);
+	time += ((uint64_t)file_time.dwHighDateTime) << 32;
+	return time / 10 / 1000;
+}
